20fe1b3d5d4855c38998ffe05e32e54beb12cec0
Tamil Arasan <tamilarasan2910@gmail.com>
2025-10-31 12:30:59 +0530

    Initial commit

---
+++ src/main/java/com/example/trip_sheet_backend/TripSheetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.trip_sheet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripSheetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripSheetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trip_sheet_backend/TripSheetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trip_sheet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripSheetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
