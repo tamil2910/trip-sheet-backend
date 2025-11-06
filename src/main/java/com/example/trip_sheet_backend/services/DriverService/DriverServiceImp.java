1cbd1490518c3019504df80ffb90138e68b2a3bd
Tamil Arasan <tamilarasan2910@gmail.com>
2025-11-06 17:54:28 +0530

    .env support command added in application.properties and @service annotation added for DriverServiceImp

--- src/main/java/com/example/trip_sheet_backend/services/DriverService/DriverServiceImp.java
+++ src/main/java/com/example/trip_sheet_backend/services/DriverService/DriverServiceImp.java
@@ -2,10 +2,13 @@ package com.example.trip_sheet_backend.services.DriverService;
 
 import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+
 import com.example.trip_sheet_backend.common.services.BaseServiceImp;
 import com.example.trip_sheet_backend.models.Driver;
 import com.example.trip_sheet_backend.repositories.DriverRepository;
 
+@Service
 public class DriverServiceImp extends BaseServiceImp<Driver, UUID> implements DriverService {
 
   public DriverServiceImp(DriverRepository repository) {
