aa94248473d3af9cacac724301b03b3d08e6d8a8
Tamil Arasan <tamilarasan2910@gmail.com>
2025-11-06 17:25:05 +0530

    drivers route is added by requestmapping

--- src/main/java/com/example/trip_sheet_backend/controllers/DriverController.java
+++ src/main/java/com/example/trip_sheet_backend/controllers/DriverController.java
@@ -2,10 +2,15 @@ package com.example.trip_sheet_backend.controllers;
 
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.trip_sheet_backend.common.controllers.BaseController;
 import com.example.trip_sheet_backend.models.Driver;
 import com.example.trip_sheet_backend.services.DriverService.DriverServiceImp;
 
+@RestController
+@RequestMapping("/drivers")
 public class DriverController extends BaseController<Driver, UUID>{
   private final DriverServiceImp service;
   public DriverController(DriverServiceImp service){
