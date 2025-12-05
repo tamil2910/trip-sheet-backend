577f4e2f5805453043299f0238190c0ab0412a37
Tamil Arasan <tamilarasan2910@gmail.com>
2025-12-05 14:09:11 +0530

    password encode has issue fixed

--- src/main/java/com/example/trip_sheet_backend/config/AppConfig.java
+++ src/main/java/com/example/trip_sheet_backend/config/AppConfig.java
@@ -3,8 +3,6 @@ package com.example.trip_sheet_backend.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
@@ -12,11 +10,6 @@ public class AppConfig {
   @Bean
   public ModelMapper modelMapper() {
       return new ModelMapper();
-  }
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-      return new BCryptPasswordEncoder();
   }
 
 }
