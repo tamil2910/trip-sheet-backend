9ed69c877f1dfb460494562aaba085534d665915
Tamil Arasan <tamilarasan2910@gmail.com>
2026-06-16 18:19:10 +0530

    passenger organisation mapping reposity issue has been fixed

--- src/main/java/com/example/trip_sheet_backend/repositories/PassengerOrganisationTenantMappingRepository.java
+++ src/main/java/com/example/trip_sheet_backend/repositories/PassengerOrganisationTenantMappingRepository.java
@@ -1,14 +1,12 @@
 package com.example.trip_sheet_backend.repositories;
 
+import com.example.trip_sheet_backend.common.repositories.BaseRepository;
 import com.example.trip_sheet_backend.models.PassengerOrganisationTenantMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
-public interface PassengerOrganisationTenantMappingRepository extends JpaRepository<PassengerOrganisationTenantMapping, UUID> {
+public interface PassengerOrganisationTenantMappingRepository extends BaseRepository<PassengerOrganisationTenantMapping, UUID> {
     Optional<PassengerOrganisationTenantMapping> findByUserAccount_IdAndOrganisation_Id(UUID userId, UUID organisationId);
     boolean existsByUserAccount_IdAndOrganisation_Id(UUID userId, UUID organisationId);
 }
